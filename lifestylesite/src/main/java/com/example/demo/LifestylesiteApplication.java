6b62fb6d235476298ef63a3da75db21bcaea9fd8
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-18 12:12:29 +0100

    initial project setup

---
+++ lifestylesite/src/main/java/com/example/demo/LifestylesiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestylesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestylesiteApplication.class, args);
+	}
+
+}
