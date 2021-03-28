e690171437d7c585962a87b29465417088953316
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-28 13:59:21 +0200

    Skapade en toDo klass som används för att skapa tasks.

---
+++ backend/lifestylesite/src/main/java/com/example/demo/model/ToDo.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+public class ToDo {
+
+    private int taskID;
+    private String Task;
+    private boolean taskStatus = false;
+
+    public ToDo(int taskID, String task) {
+        this.taskID = taskID;
+        Task = task;
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public String getTask() {
+        return Task;
+    }
+}
