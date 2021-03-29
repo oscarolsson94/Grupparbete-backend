888c924bb1c6545c8fd6d7c9e3492beaddf43b32
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-29 14:05:43 +0200

    changing path to index page to "/"

--- backend/lifestylesite/src/main/java/com/example/demo/controllers/Start.java
+++ backend/lifestylesite/src/main/java/com/example/demo/controllers/Start.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Start {
 
-    @GetMapping("/start")
+    @GetMapping("/")
     public String start(){
         return "start";
     }
