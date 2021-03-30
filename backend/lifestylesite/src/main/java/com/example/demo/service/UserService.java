dd09c6a9c5cd95f6ad13a0b71875637cb00b6e2c
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-30 12:27:47 +0200

    adding userDetailsService - need spring security

--- backend/lifestylesite/src/main/java/com/example/demo/service/UserService.java
+++ backend/lifestylesite/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,10 @@
 package com.example.demo.service;
 
-public interface UserService {
+import com.example.demo.dto.UserRegisterDto;
+import com.example.demo.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
+public interface UserService extends UserDetailsService { // extend needed for spring security
+
+	User save(UserRegisterDto registrationDto); //declare method that will save data
 }
