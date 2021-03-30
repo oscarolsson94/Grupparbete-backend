6089fe9f4ff162d1b5f1fbfb2cc3baaaaa5cf2b0
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-30 14:47:18 +0200

    fixed bug where table can't be called "user" in postgres..

--- backend/lifestylesite/src/main/java/com/example/demo/model/User.java
+++ backend/lifestylesite/src/main/java/com/example/demo/model/User.java
@@ -16,7 +16,7 @@ import javax.persistence.UniqueConstraint;
 import javax.persistence.JoinColumn;
 
 @Entity
-@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 
 	@Id
