22b1f7dca4e320d31ad074fa67055e1336c1f443
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-31 13:11:56 +0200

    Skapade en ToDoAPI

---
+++ backend/lifestylesite/src/main/java/com/example/demo/api/ToDoAPI.java
@@ -0,0 +1,11 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todos")
+public class ToDoAPI {
+
+
+}
