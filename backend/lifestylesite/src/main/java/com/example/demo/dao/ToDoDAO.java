b9228fb944549246f8eb40a079abfbe1a1db0950
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-31 14:48:33 +0200

    Tog bort en del kod i ToDo samt i ToDoDAO

--- backend/lifestylesite/src/main/java/com/example/demo/dao/ToDoDAO.java
+++ backend/lifestylesite/src/main/java/com/example/demo/dao/ToDoDAO.java
@@ -1,21 +1,8 @@
 package com.example.demo.dao;
-
-import com.example.demo.repository.ToDoRepository;
 import org.springframework.stereotype.Repository;
 
 //Ett repository är något som interagerar med databasen.
 @Repository
 public class ToDoDAO {
 
-
-    final ToDoRepository repository;
-
-    public ToDoDAO(ToDoRepository repository) {
-        this.repository = repository;
-    }
-
-    //I denna metod så sparar jag instansen av klassen ToDoDAO till databasen.
-    public void addToDo(ToDoDAO toDoDAO){
-        repository.save(toDoDAO);
-    }
 }
