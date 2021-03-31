4b6c271b87ce4cc393ba66cea38207eec7dfb4ff
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-31 15:21:05 +0200

    Skapade en metod som returnerar en lista med alla ToDo objekt i databasen.

--- backend/lifestylesite/src/main/java/com/example/demo/api/ToDoAPI.java
+++ backend/lifestylesite/src/main/java/com/example/demo/api/ToDoAPI.java
@@ -1,12 +1,27 @@
 package com.example.demo.api;
 
+import com.example.demo.model.ToDo;
+import com.example.demo.repository.ToDoRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos")
 public class ToDoAPI {
 
+    @Autowired
+    ToDoRepository toDoRepository;
+
+    @GetMapping("/")
+    public List<ToDo> getAllToDos(){
+        List<ToDo> toDoList = toDoRepository.findAll();
+        return toDoList;
+    }
 
 }
