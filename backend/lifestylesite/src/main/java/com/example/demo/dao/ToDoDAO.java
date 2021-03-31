d6ee145135199f4de9e041ba9200f1c062e505d8
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-31 13:54:24 +0200

    skapade en addToDo metod som sparar en instans av ToDoDAO i databasen

--- backend/lifestylesite/src/main/java/com/example/demo/dao/ToDoDAO.java
+++ backend/lifestylesite/src/main/java/com/example/demo/dao/ToDoDAO.java
@@ -1,9 +1,20 @@
 package com.example.demo.dao;
 
+import com.example.demo.repository.ToDoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ToDoDAO {
-    
 
+
+    final ToDoRepository repository;
+
+    public ToDoDAO(ToDoRepository repository) {
+        this.repository = repository;
+    }
+
+    //I denna metod så sparar jag instansen av klassen ToDoDAO till databasen.
+    public void addToDo(ToDoDAO toDoDAO){
+        repository.save(toDoDAO);
+    }
 }
