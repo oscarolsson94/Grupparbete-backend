19d67477bbc5d47725fc0b300f2f2cce200fa44f
AdrianCo97 <adrian.corazza@outlook.com>
2021-04-09 17:38:01 +0200

    Generated a test class

---
+++ lifestylesite/src/test/java/com/example/demo/service/ToDoServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoServiceTest {
+
+    @Test
+    void addToDoToDatabase() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void allToDoByMail() {
+    }
+}
