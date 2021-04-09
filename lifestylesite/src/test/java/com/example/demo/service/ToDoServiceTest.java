04f0f64ef3a01d445733c04af81c76b76382e6e7
AdrianCo97 <adrian.corazza@outlook.com>
2021-04-09 17:55:15 +0200

    Made a mock of the ToDoDao class

--- lifestylesite/src/test/java/com/example/demo/service/ToDoServiceTest.java
+++ lifestylesite/src/test/java/com/example/demo/service/ToDoServiceTest.java
@@ -1,13 +1,27 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.ToDoDao;
+import com.example.demo.model.ToDo;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToDoServiceTest {
 
+    static ToDoDao toDoDao;
+
+    @BeforeAll
+    public static void start(){
+        toDoDao = Mockito.mock(ToDoDao.class);
+        //Here we set up an "fake" version of the dao class,
+        //This means we can throw certain tests at the database without it actually running.
+    }
+
     @Test
     void addToDoToDatabase() {
+
     }
 
     @Test
