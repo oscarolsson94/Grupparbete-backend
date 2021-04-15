054e27b5451d6d604c7fb7fe7e72813b174250c7
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2021-04-15 02:06:35 +0200

    a lot of styling and thymeleaf

--- lifestylesite/src/main/java/com/example/demo/controllers/CalenderController.java
+++ lifestylesite/src/main/java/com/example/demo/controllers/CalenderController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.Calender;
 import com.example.demo.service.CalenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CalenderController {
@@ -20,6 +25,7 @@ public class CalenderController {
     @RequestMapping("calendar")
     public String getCalender(Model model){
        model.addAttribute("calendarslist", calenderService.initializeCalenders());
+       model.addAttribute("calendar", new Calender(LocalDate.now()));
           return "calendar";
     }
 
